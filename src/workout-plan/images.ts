a41df1a9ed365136f9ab320a54e273f1313ff3c3
Akshay Choudhry <mail@akshayc.dev>
2024-06-19 17:35:30 +0200

    add thursdays images

--- src/workout-plan/images.ts
+++ src/workout-plan/images.ts
@@ -2,12 +2,16 @@ import benchPressImage from "../assets/bench_press_barbell.png";
 import benchDips from "../assets/bench-dips-on-floor.png";
 import bentOverRows from "../assets/Bent-over-barbell-row.png";
 import bicepCurl from "../assets/bicepCurl.png";
+import calfRaise from "../assets/calfRaise.png";
 import concentrationCurl from "../assets/concentrationCurl.png";
 import deadlifts from "../assets/deadlifts.png";
 import dumbellFlyes from "../assets/dumbell_flyes2.png";
 import hammerCurl from "../assets/hammerCurl.png";
 import inclineDumbellPress from "../assets/incline_dumbell_press.png";
+import lunges from "../assets/lunges.png";
+import rdl from "../assets/rdl.png";
 import singleArmDumbellRow from "../assets/singleArmDumbellRow.png";
+import squats from "../assets/squats.png";
 import tricepExtensions from "../assets/tricep-extensions.png";
 import tricepKickback from "../assets/tricep-kickback.png";
 
@@ -16,12 +20,16 @@ export const exerciseImages: { [key: string]: string } = {
   benchDips: benchDips,
   bentOverRows: bentOverRows,
   bicepCurl: bicepCurl,
+  calfRaise: calfRaise,
   concentrationCurl: concentrationCurl,
   deadlifts: deadlifts,
   dumbellFlyes: dumbellFlyes,
   hammerCurl: hammerCurl,
   inclineDumbellPress: inclineDumbellPress,
+  lunges: lunges,
+  rdl: rdl,
   singleArmDumbellRow: singleArmDumbellRow,
+  squats: squats,
   tricepExtensions: tricepExtensions,
   tricepKickback: tricepKickback,
 };
